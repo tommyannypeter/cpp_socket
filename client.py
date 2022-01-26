dead965e6fb4e691bf0597e12da7b7c6de2e60c3
Po-Shan Shih <psgo06031@gmail.com>
2022-01-26 17:11:46 +0800

    client send/recv msg

--- client.py
+++ client.py
@@ -2,4 +2,6 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 13579))
+print(client.recv(1024))
+client.sendall("I'm here!")
 client.close()
