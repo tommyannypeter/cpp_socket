0e073b361782b7ba92e9281ae7b63ff160726514
Po-Shan Shih <psgo06031@gmail.com>
2022-02-08 14:55:54 +0800

    typo

--- SlaveSocket.cc
+++ SlaveSocket.cc
@@ -14,7 +14,7 @@ void SlaveSocket::run() {
     while (true) {
         m_delegate.put_in_string(receive_string());
         m_delegate.callback();
-        if (m_delegate.will_reply()) send_string(m_delegate.put_out_string());
+        if (m_delegate.will_reply()) send_string(m_delegate.take_out_string());
         if (m_delegate.is_finished()) break;
     }
 }
